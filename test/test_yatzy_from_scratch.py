54e14b3fee9102510d903f9106674a6ad4cb448a
ncocana <ncocana@cifpfbmoll.eu>
2022-12-31 19:54:29 +0100

    test(test_yatzy_from_scratch.py): add test case for yatzy

--- test/test_yatzy_from_scratch.py
+++ test/test_yatzy_from_scratch.py
@@ -4,13 +4,32 @@ from src.yatzy import Yatzy
 # Chance
 # The player scores the sum of all dice, no matter what they read.
 
-
+@pytest.mark.testChance
 def test_chance():
+    '''
+    Chance:
+    The player scores the sum of all dice, no matter what they read.
+    '''
+
     # iterar sobre *args evita codigo cableado a 5 argumentos
     assert 15 == Yatzy.chance(1, 2, 3, 4, 5)
     assert 14 == Yatzy.chance(1, 1, 3, 3, 6)
     assert 21 == Yatzy.chance(4, 5, 5, 6, 1)
 
+def test_yatzy():
+    '''
+    Yatzy:
+    If all dice have the same number, the player scores 50 points.
+    '''
+    
+    # dice significa "dados" pero exige un unico argumento
+    # => interfaz abstraccion del metodo no es coherente
+    # con el resto de metodos
+    # El codigo para iterar sobre dice es muy complejo
+    # El algoritmo para averiguar si todos los dados son iguales
+    # es muy complejo
+    assert 50 == Yatzy.yatzy(1, 1, 1, 1, 1)
+    assert 0 == Yatzy.yatzy(1, 1, 1, 2, 1)
 
 @pytest.fixture
 def inyector():
@@ -19,6 +38,7 @@ def inyector():
     return tirada
 
 
+@pytest.mark.testFours
 def test_fours(inyector):
     # Es necesario un objeto ya creado
     valorEsperado = 4
